004e2082e384d88f534c3302caab860adc39bb9f
Ana Vi <dismal.damsel@gmail.com>
2025-08-08 15:47:51 +0300

    Refactor MoodEntry model with emotion scale and specific tags, and set dashboard as login redirect

--- moods/models.py
+++ moods/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class MoodEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    mood = models.IntegerField(choices=[(i, i) for i in range(1,11)])
-    emotion_tags = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(default=timezone.now)
     
+    happy = models.IntegerField(choices=[(i, i) for i in range(1,11)])
+    angry = models.IntegerField(choices=[(i, i) for i in range(1,11)])
+    sad = models.IntegerField(choices=[(i, i) for i in range(1,11)])
+    fear = models.IntegerField(choices=[(i, i) for i in range(1,11)])
+    disgust = models.IntegerField(choices=[(i, i) for i in range(1,11)])
+    surprise = models.IntegerField(choices=[(i, i) for i in range(1,11)])
+    
+    happy_tag = models.CharField(max_length=50, default='neutral')
+    angry_tag = models.CharField(max_length=50, default='neutral')
+    sad_tag = models.CharField(max_length=50, default='neutral')
+    fear_tag = models.CharField(max_length=50, default='neutral')
+    disgust_tag = models.CharField(max_length=50, default='neutral')
+    surprise_tag = models.CharField(max_length=50, default='neutral')
+       
     def __str__(self):
-        return f"{self.user.username} - {self.mood} on {self.timestamp.strftime('%d-%m-%Y')}"
+        return f"{self.user.username} on {self.timestamp.strftime('%d-%m-%Y')}"
