e561a701bcb036b9f8d207fe067d0977dc88cfcc
Ana Vi <dismal.damsel@gmail.com>
2025-08-25 17:32:34 +0300

    Refactor: DRY MoodEntryForm by dinamically generating emotion tag fields, set default values for emotions, and add paragraphs for user guidance

--- moods/forms.py
+++ moods/forms.py
@@ -10,14 +10,18 @@ EMOTION_TAG_OPTIONS = {
     'surprise': ['shocked', 'curious', 'amazed'],
 }
 
-class MoodEntryForm(forms.ModelForm):
-    happy_tag = forms.ChoiceField(choices=[('neutral', 'neutral')] + [(tag, tag) for tag in EMOTION_TAG_OPTIONS['happy']], required=False)
-    angry_tag = forms.ChoiceField(choices=[('neutral', 'neutral')] + [(tag, tag) for tag in EMOTION_TAG_OPTIONS['angry']], required=False)
-    sad_tag = forms.ChoiceField(choices=[('neutral', 'neutral')] + [(tag, tag) for tag in EMOTION_TAG_OPTIONS['sad']], required=False)
-    fear_tag = forms.ChoiceField(choices=[('neutral', 'neutral')] + [(tag, tag) for tag in EMOTION_TAG_OPTIONS['fear']], required=False)
-    disgust_tag = forms.ChoiceField(choices=[('neutral', 'neutral')] + [(tag, tag) for tag in EMOTION_TAG_OPTIONS['disgust']], required=False)
-    surprise_tag = forms.ChoiceField(choices=[('neutral', 'neutral')] + [(tag, tag) for tag in EMOTION_TAG_OPTIONS['surprise']], required=False)
-    
+class MoodEntryForm(forms.ModelForm):    
     class Meta:
         model = MoodEntry
-        fields = ['happy', 'happy_tag', 'angry', 'angry_tag', 'sad', 'sad_tag', 'fear', 'fear_tag', 'disgust', 'disgust_tag', 'surprise', 'surprise_tag']
+        fields = ['happy', 'angry', 'sad', 'fear', 'disgust', 'surprise']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for emotion in ['happy', 'angry', 'sad', 'fear', 'disgust', 'surprise']:
+            self.fields[emotion].initial = 4
+        
+        for emotion, tags in EMOTION_TAG_OPTIONS.items():
+            field_name = f"{emotion}_tag"
+            choices = [('neutral', 'neutral')] + [(tag, tag) for tag in tags]
+            self.fields[field_name] = forms.ChoiceField(choices=choices, required=False)
